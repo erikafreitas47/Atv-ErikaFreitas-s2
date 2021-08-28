7278a2bb5c00bc11acd159f2a559dba17f363dc0
Érika de Freitas <88119150+erikafreitas47@users.noreply.github.com>
2021-08-28 14:01:02 -0300

    Add files via upload

---
+++ erika.js
@@ -0,0 +1,7 @@
+alert("Bem vinda(o) ao meu site!");
+    var soma = (87+96);
+alert(soma + " é a soma de 87 + 96!");
+    var multiplicacao = (5 * 8);
+alert("O resultado da multiplicação entre 5 e 8 é de " + multiplicacao + "!");
+    var listaMercado = ["Brócolis", " abóbora", " espinafre."];
+alert("Vamos ao mercado comprar os seguintes itens: " + listaMercado); 
